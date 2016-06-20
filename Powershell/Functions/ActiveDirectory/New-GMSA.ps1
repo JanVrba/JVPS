c5cc1164dc975f07487765600a62da35d4a8b18a
JanVrba <jan.vrba@gmx.co.uk>
2016-06-20 12:14:01 +0100

    New Function New-GMSA

---
+++ Powershell/Functions/ActiveDirectory/New-GMSA.ps1
@@ -0,0 +1,34 @@
+# Name : New-GMSA
+# Date : 02/06/2016
+# Purpose : Function creates new group management service account at AD and return SAM account name
+
+function New-GMSA {
+
+    [CmdletBinding()]
+    param(
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()] 
+		[String]
+		$FirmName,
+
+		[Parameter(Mandatory=$true)]
+		[String]
+		$gMSA_Prefix,
+
+		[Parameter(Mandatory=$true)]
+		[String]
+		$Domain,
+
+		[Parameter(Mandatory=$true)]
+		[String]
+		$ADSecurityGroup
+	)
+
+	$gMSA = ($gMSA_Prefix + '_' + $FirmName)
+	$gMSAFQDN = $gMSA + '.' + $Domain
+
+New-ADServiceAccount -Name $gMSA -DNSHostName $gMSAFQDN -PrincipalsAllowedToRetrieveManagedPassword $ADSecurityGroup
+$NewGMSA = (Get-ADServiceAccount -Identity $gMSA).SamAccountName
+return $NewGMSA
+
+} # close function
