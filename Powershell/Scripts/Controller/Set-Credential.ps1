63a6fb0f95d28bcc9254e6ee44032faec4336124
JanVrba <jan.vrba@gmx.co.uk>
2016-06-20 14:14:35 +0100

    New function Set-Credential

---
+++ Powershell/Scripts/Controller/Set-Credential.ps1
@@ -0,0 +1,25 @@
+# Name : Set-Credential
+# Date : 02/06/2016
+# Purpose : Function creates Cred object which includes username and password
+
+function Set-Credential {
+
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [ValidateNotNullOrEmpty()] 
+		[String]
+        $Username, 	
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()] 
+		[String]
+		$Password
+
+    ) # close param
+    
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+$Cred
+
+} # close Function
