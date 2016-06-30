a6102ce8d37e85cb5e7fae88a397f818e8d46173
JanVrba <jan.vrba@gmx.co.uk>
2016-06-30 10:10:05 +0100

    New Script ArchiveNugetPackages.ps1

---
+++ Powershell/Scripts/ArchiveNugetPackages.ps1
@@ -0,0 +1,13 @@
+# Name : ArchiveNuGetPackages.ps1
+# Date : 30/06/2016
+# Purpose : Script Archive Old version of NuGet packages
+
+$source = "\\cc1\Bin\GBP\NuGet\master"
+$archivePath = \\cc1\archive
+$retentionTime = (get-date).AddDays(-31)
+
+Get-Childitem -Path $source | 
+    Where-Object {$_.LastWriteTime -lt $retentionTime} |
+    ForEach {
+        Move-Item $_.FullName -destination $archivePath -force -ErrorAction:SilentlyContinue
+    } # close ForEach
