76dd63e78df389a098dee10e45c0665ed4ad22a8
JanVrba <jan.vrba@gmx.co.uk>
2016-07-13 12:04:29 +0100

    New function Edit-OctopusProjectVariable.ps1

---
+++ Powershell/Functions/DevOps/Octopus/Edit-OctopusProjectVariable.ps1
@@ -0,0 +1,58 @@
+# Name : Edit-OctopusProjectVariable.ps1
+# Date : 13/07/2016
+# Purpose : Function change value of Octopus project varible using Octopus Client .NET
+# Inspired by : https://github.com/OctopusDeploy/OctopusDeploy-Api/blob/master/Octopus.Client/PowerShell/Variables/UpdateVariableInProject.ps1
+
+Function Edit-OctopusProjectVariable{
+[CmdletBinding()]
+Param(
+    [Parameter()]
+    [string]$octopusURL,
+     
+    [Parameter()]
+    [string]$octopusAPIKey,
+
+    [Parameter()]
+    [string]$projectName,
+
+    [Parameter()]
+    [string]$varName,
+
+    [Parameter()]
+    [string]$newValue
+
+    )
+
+
+# if is called  from Octopus server
+
+Add-Type -Path "C:\Program Files\Octopus Deploy\Octopus\Newtonsoft.Json.dll"
+Add-Type -Path "C:\Program Files\Octopus Deploy\Octopus\Octopus.Client.dll" 
+
+<#  or if from Tentacle
+Add-Type -Path "C:\Program Files\Octopus Deploy\Tentacle\Newtonsoft.Json.dll"
+Add-Type -Path "C:\Program Files\Octopus Deploy\Tentacle\Octopus.Client.dll"
+ #>
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint ($octopusURL, $octopusAPIKey)
+$octoRepo = new-object Octopus.Client.OctopusRepository $endpoint
+
+#Get Project
+$project = $octoRepo.Projects.FindByName($projectName)
+
+#Get Project's variable set
+$variableSet = $octoRepo.VariableSets.Get($project.links.variables)
+
+#Get variable to update  
+$variable = $variableSet.Variables | Where-Object {$_.name -eq $varName}
+
+#Update variable
+$variable.Value = $newValue
+$Variable.IsSensitive = $false #Set to $true if you want to treat this variable as sensitive 
+
+#Save variable set
+$octoRepo.VariableSets.Modify($variableSet) | out-null
+
+Write-Information "$($variable.Name):Value = $($variable.Value) "
+
+} # close function
