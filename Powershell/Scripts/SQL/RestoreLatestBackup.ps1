e236c4083f66053772a6cae52ea94b9db7cc84fe
JanVrba <jan.vrba@gmx.co.uk>
2016-07-15 15:19:19 +0100

    Update Script RestoreLatestBackup.ps1

--- Powershell/Scripts/SQL/RestoreLatestBackup.ps1
+++ Powershell/Scripts/SQL/RestoreLatestBackup.ps1
@@ -1,21 +1,80 @@
-# Name : RestoreLatestBackup.ps1
-# Date : 06/07/2016
-# Purpose : Script will use latest backup and perform restore
-
+# Name : AH.RestoreDBCheck.ps1
+# Date : 07/07/2016
+# Purpose : Script Restore last Full backup, diff + log backups to TEST SQL server and CheckDB
+# Inspired by : https://www.simple-talk.com/sql/backup-and-recovery/backup-and-restore-sql-server-with-the-sql-server-2012-powershell-cmdlets/
 
 Import-Module "SQLPS" -DisableNameChecking
-$backupPath = "C:\SQLBackup"
-$backupFile = (Get-ChildItem -path $backupPath -Filter "*.bak" | 
+
+# set Environment variables
+
+$backupPath = ""
+$database = ""
+$sqlserver = ""
+$backupPathFull = Join-Path $backupPath -ChildPath $database
+$relocationPath = ""
+
+# get backup files
+
+$backupFileFull = (Get-ChildItem -path $backupPathFull -Filter "*.bak" | 
     Where-Object {-not $_.PSIsContainer} | 
-    Sort-Object -Property $_.creationtime | 
+    Sort-Object LastWriteTime | 
     Select-Object -last 1).Name
 
-$backupFilePath = Join-path $backupPath -ChildPath $backupFile
+$backupfileDiff = (Get-ChildItem -path $backupPathFull -Filter "*.diff" | 
+    Where-Object {-not $_.PSIsContainer} | 
+    Sort-Object LastWriteTime | 
+    Select-Object -last 1).Name
 
-$restoreToDatabase =  "Apply4law3TEST"
-$dbserver = "test-wc\sqlexpress"
+$backupfileDiffLastWriteTime = (Get-ChildItem -path $backupPathFull -Filter "*.diff" | 
+    Where-Object {-not $_.PSIsContainer} | 
+    Sort-Object LastWriteTime | 
+    Select-Object -last 1).LastWriteTime
 
-$mdf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Apply4law3", "C:\SQLBACKUP\Apply4law3TEST.mdf")
-$ldf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Apply4law3_Log", "C:\SQLBACKUP\Apply4law3TEST.Apply4law3TEST_Log.ldf")
+# get array of *.trn files
+$backupfileLog = Get-ChildItem -path $backupPathFull -Filter "*.trn" | 
+    Where-Object {-not $_.PSIsContainer} | 
+    Where-Object {$_.LastWriteTime -gt $backupfileDiffLastWriteTime} |
+    Sort-Object LastWriteTime
+   
+# full paths to backup files
 
-Restore-SqlDatabase -Database $restoreToDatabase -BackupFile $backupfilepath -ServerInstance $dbserver -RelocateFile @($mdf,$ldf)
+$backupFileFullPath = Join-path $backupPathFull -ChildPath $backupFileFull
+$backupFileDiffPath = Join-path $backupPathFull -ChildPath $backupFileDiff
+
+# restore to time of last TranLog file
+
+$restorePointTime = ($backupfileLog | select -last 1).LastWriteTime
+
+# relocate files 
+
+$mdfFilePath = Join-Path $relocationPath -ChildPath "$database.mdf"
+$ldfFilePath = Join-Path $relocationPath -ChildPath "$database`_log.ldf"
+ 
+$mdf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($database, $mdfFilePath)
+$ldf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("$database`_log" , $ldfFilePath)
+
+# Restore Last Full Backup
+
+Restore-SqlDatabase -Database $database  -BackupFile $backupfileFullpath -ServerInstance $sqlserver -ReplaceDatabase -RelocateFile($mdf,$ldf) -NoRecovery -Verbose
+
+# Restore Last Diff Backup
+
+Restore-SqlDatabase -Database $database -BackupFile $backupfileDiffpath -ServerInstance $sqlserver -ReplaceDatabase -NoRecovery -Verbose
+
+# Restore Transaction Log Backups
+
+$recovery = 0
+foreach ($file in $backupfileLog) {
+    $backupFileLogPath = Join-path $backupPathFull -ChildPath $file.Name
+    if ($file.LastWriteTime -lt $restorePointTime) {
+            Restore-SqlDatabase -Database $database -BackupFile $backupFileLogPath -ServerInstance $sqlserver -ReplaceDatabase -NoRecovery -Verbose
+    } else {
+       if ($recovery -eq 0) {
+            Restore-SqlDatabase -Database $database -BackupFile $backupFileLogPath -ServerInstance $sqlserver -ReplaceDatabase -ToPointInTime $restorePointTime -Verbose
+            $recovery = 1       
+       } # close if Recovery
+    } # close if NoRecovery
+} # close foreach
+
+# DB CHECK
+# Invoke-sqlcmd -ServerInstance $sqlserver -Query "DBCC CHECKDB ($database);" -Verbose #>
