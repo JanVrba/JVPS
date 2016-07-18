7ea7b1db680e1e51761025dc44477e771d17d4d7
JanVrba <jan.vrba@gmx.co.uk>
2016-07-18 16:47:42 +0100

    Update Script BackupFileCreationTask.ps1

--- Powershell/Scripts/Admin/ScheduledTask/BackupFileCreationTask.ps1
+++ Powershell/Scripts/Admin/ScheduledTask/BackupFileCreationTask.ps1
@@ -1,4 +1,4 @@
-# Name : BackupFileCreation.Task.ps1
+# Name : AH.BackupFileCreation.Setup.ps1
 # Date : 11/07/2016
 # Purpose : Script Set Scheduled task for check of backup file creation.
 
@@ -6,13 +6,14 @@
 $taskAction = [ordered]@{
 
     Execute = "Powershell.exe"
-    Argument =   '-NoProfile -WindowStyle Hidden -File "C:\websites\www.allhires.com\AHBackup\AH.BackupFileCreationCheck.ps1"'   
+    Argument =   '-NoProfile -WindowStyle Hidden -File "{[ps1 file path]}"'   
   
 } # close taskAction
 
 $action = New-ScheduledTaskAction @taskAction
 $timeSpanInterval =  New-TimeSpan -Minutes 5
 $timeSpanDuration = ([TimeSpan]::MaxValue) 
-$trigger = New-ScheduledTaskTrigger -RepetitionInterval $timeSpan -Once -At "00:00" -RepetitionDuration $timeSpanDuration
+$trigger = New-ScheduledTaskTrigger -RepetitionInterval $timeSpanInterval -Once -At "00:00" -RepetitionDuration $timeSpanDuration
+$principal = New-ScheduledTaskPrincipal -UserID gbp\gAHBackup$ -LogonType Password
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AH.BackupFileCreationCheck" -Description "Task check every 5 minutes if Transaction log file was created, latest 150 seconds ago and sent email if not"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AH.BackupFileCreationCheck" -Description "Task check every 5 minutes if Transaction log file was created, latest 150 seconds ago and sent email if not" -Principal $principal
