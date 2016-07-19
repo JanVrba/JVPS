8f643188629ea4f0e15d916385894789c7758575
JanVrba <jan.vrba@gmx.co.uk>
2016-07-19 13:27:24 +0100

    Update script ArchiveNugetPackages.ps1

--- Powershell/Solutions/GBMG/Scripts/ArchiveNugetPackages.ps1
+++ Powershell/Solutions/GBMG/Scripts/ArchiveNugetPackages.ps1
@@ -2,12 +2,34 @@
 # Date : 30/06/2016
 # Purpose : Script Archive Old version of NuGet packages
 
-$source = "\\cc1\Bin\GBP\NuGet\master"
-$archivePath = "\\cc1\archive"
-$retentionTime = (get-date).AddDays(-31)
+# environment variables
 
-Get-Childitem -Path $source | 
+$sourceFolderPath = "\\cc1\Bin\GBP\NuGet\master"
+$archiveFolderPath = "\\cc1\archive"
+$retentionTimeDays = 31
+
+Function Backup-NuGetPackageToArchive {
+    [CmdletBinding()]
+
+    param (
+    [Parameter()]
+    [string]$sourceFolderPath,
+     
+    [Parameter()]
+    [string]$archiveFolderPath,
+
+    [Parameter()]
+    [int]$retentionTimeDays
+    ) # close param
+
+$retentionTime = (get-date).AddDays(-$retentionTimeDays)
+
+Get-Childitem -Path $sourceFolderPath | 
     Where-Object {$_.LastWriteTime -lt $retentionTime} |
         ForEach {
-            Move-Item $_.FullName -destination $archivePath -force -ErrorAction:SilentlyContinue
+            Move-Item $_.FullName -destination $archiveFolderPath -force -ErrorAction:SilentlyContinue
         } # close ForEach
+
+} # close Function
+
+Backup-NuGetPackageToArchive -sourceFolderPath $sourceFolderPath -archiveFolderPath $archiveFolderPath -retentionTimeDays $retentionTimeDays
