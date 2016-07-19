4cea173369a97f535ea3fb1e160a508f6a135d4f
JanVrba <jan.vrba@gmx.co.uk>
2016-07-19 15:30:06 +0100

    Update ArchiveNugutPackages.ps1

--- Powershell/Solutions/GBMG/Scripts/ArchiveNugetPackages.ps1
+++ Powershell/Solutions/GBMG/Scripts/ArchiveNugetPackages.ps1
@@ -1,12 +1,18 @@
 # Name : ArchiveNuGetPackages.ps1
 # Date : 30/06/2016
 # Purpose : Script Archive Old version of NuGet packages
+# JIRA : http://jira1.intranet.gbp.co.uk/browse/DO-32
 
 # environment variables
 
 $sourceFolderPath = "\\cc1\Bin\GBP\NuGet\master"
-$archiveFolderPath = "\\cc1\archive"
+$archiveFolderPath = "\\cc1\Bin\GBP\NuGet\Archive"
 $retentionTimeDays = 31
+
+$packagesGroups = Get-ChildItem $sourceFolderPath | ForEach-Object {($_.Name.Split(".")[0] + $_.Name.Split(".")[1])} | group
+
+$packagesGroups | Where-Object {$_.count -gt 5} | sort-object Count -Descending
+
 
 Function Backup-NuGetPackageToArchive {
     [CmdletBinding()]
