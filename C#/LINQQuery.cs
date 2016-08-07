c18e2a235c52ec1fca99144badbaf358206fc813
Jan Vrba <Jan Vrba>
2016-08-07 12:43:17 +0100

    new C# LINQQuery.cs

---
+++ C#/LINQQuery.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> myCars = new List<Car> {
+                new Car { Name = "Ferrari" }  ,
+                new Car { Name = "Lamborgini" }
+            };
+
+            var ferrari = from car in myCars
+                          where car.Name == "Ferrari"
+                          select car;
+
+            foreach (var car in ferrari)
+            {
+                Console.WriteLine("{0}", car.Name);
+            }
+
+            Console.ReadLine();
+            
+        }
+    };
+
+    class Car
+    {
+        public string Name { get; set; }
+    }
+}
